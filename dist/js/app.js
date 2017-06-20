2c0360c8e1efe77810e5843f6904350ef6fa768b
Dylan Ludwick <DylanRossLudwick@Dylans-MacBook-Pro.local>
2017-06-19 22:34:55 -0600

    add save bookmark functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,22 @@
 // listen for form submit
 document.getElementById('form-a').addEventListener('submit', saveBookmark);
 
-function saveBookmark() {
+function saveBookmark(e) {
   console.log('It Works!');
+
+  // Get form values
+  var siteName = document.getElementById('siteName').value;
+  console.log(siteName);
+  var siteUrl = document.getElementById('siteUrl').value;
+  console.log(siteUrl);
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
+
+  console.log(bookmark);
+
+  // Prevent form from submitting
+  e.preventDefault();
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
 // listen for form submit
 document.getElementById('form-a').addEventListener('submit', saveBookmark);
 
-function saveBookmark() {
+function saveBookmark(e) {
   console.log('It Works!');
+
+  // Get form values
+  const siteName = document.getElementById('siteName').value;
+  console.log(siteName);
+  const siteUrl = document.getElementById('siteUrl').value;
+  console.log(siteUrl);
+
+  const bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  console.log(bookmark);
+
+  // Prevent form from submitting
+  e.preventDefault();
 }
