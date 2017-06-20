46410960afc93d6dcc0a97860e11332595c1be95
Dylan Ludwick <DylanRossLudwick@Dylans-MacBook-Pro.local>
2017-06-19 23:01:06 -0600

    edit saveBookmark to set to LocalStorage

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,13 +5,13 @@ document.getElementById('form-a').addEventListener('submit', saveBookmark);
 
 // Save bookmark
 function saveBookmark(e) {
-  console.log('It Works!');
+  //console.log('It Works!');
 
   // Get form values
   var siteName = document.getElementById('siteName').value;
-  console.log(siteName);
+  //console.log(siteName);
   var siteUrl = document.getElementById('siteUrl').value;
-  console.log(siteUrl);
+  //console.log(siteUrl);
 
   var bookmark = {
     name: siteName,
@@ -20,11 +20,30 @@ function saveBookmark(e) {
 
   console.log(bookmark);
 
-  // LocalStorage test
-  localStorage.setItem('test', 'Hello World');
-  console.log(localStorage.getItem('test'));
-  localStorage.removeItem('test');
-  console.log(localStorage.getItem('test'));
+  /*
+    // LocalStorage test
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+  */
+
+  // Test if bookmark is null
+  if (localStorage.getItem('bookmarks') === null) {
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to LocalStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from LocalStorage
+    var _bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    _bookmarks.push(bookmark);
+    // Re-set back to LocalStorage
+    localStorage.setItem('bookmarks', JSON.stringify(_bookmarks));
+  }
 
   // Prevent form from submitting
   e.preventDefault();
--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,13 @@ document.getElementById('form-a').addEventListener('submit', saveBookmark);
 
 // Save bookmark
 function saveBookmark(e) {
-  console.log('It Works!');
+  //console.log('It Works!');
 
   // Get form values
   const siteName = document.getElementById('siteName').value;
-  console.log(siteName);
+  //console.log(siteName);
   const siteUrl = document.getElementById('siteUrl').value;
-  console.log(siteUrl);
+  //console.log(siteUrl);
 
   const bookmark = {
     name: siteName,
@@ -25,6 +25,24 @@ function saveBookmark(e) {
     localStorage.removeItem('test');
     console.log(localStorage.getItem('test'));
   */
+
+  // Test if bookmark is null
+  if (localStorage.getItem('bookmarks') === null) {
+    // Init array
+    let bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to LocalStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  else {
+    // Get bookmarks from LocalStorage
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Re-set back to LocalStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
 
   // Prevent form from submitting
   e.preventDefault();
